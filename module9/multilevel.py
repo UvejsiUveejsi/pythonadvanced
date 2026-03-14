6029443be3263098aee6934a0365692234e47fd2
UvejsiUveejsi <uvejsborovci1@gmail.com>
2026-03-14 12:44:07 +0100

    Update multilevel.py

--- module9/multilevel.py
+++ module9/multilevel.py
@@ -4,5 +4,27 @@ class Vehicle:
         self.model = model
         self.year = year
 
+    def display_info(self):
+        print(f"Make: {self.make}, Model: {self.model}, Year: {self.year}")
+
+
+class Car(Vehicle):
+    def __init__(self, make, model, year, body_style):
+        super().__init__(make, model, year)
+        self.body_style = body_style
+
+
+class ElectricalCar(Car):
+    def __init__(self, make, model, year, body_style, battery_capacity):
+        super().__init__(make, model, year, body_style)
+        self.battery_capacity = battery_capacity
+
+    def charger(self):
+        print("charging currently")
+
+
+tesla = ElectricalCar("Tesla", "Cybertruck", "2023", "Triangular", 1223)
+tesla.display_info()
+print(tesla.body_style)
 print(tesla.battery_capacity)
 tesla.charger()
